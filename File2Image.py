2e75facd864b9022896c117b8fde34906cc09791
NDevTK <31563761+NDevTK@users.noreply.github.com>
2018-08-22 17:50:13 +0100

    Added silent option and large files get split

--- File2Image.py
+++ File2Image.py
@@ -3,25 +3,35 @@ from PIL import Image
 from sys import argv
 
 # Get user input
-# File2Image.py <filename> <output> <8K>
+# File2Image.py <filename> <output> <8K> silent
 if len(argv) < 2:
     fname = input("File name: ")
 else:
     fname = argv[1]
 if len(argv) < 3:
-    opname = input("Output image name: ")
+    opname = input("Output file name (Large files may get split up): ")
 else:
     opname = argv[2]
 
 mode = "8K" # resolution to create image
 # 8K allows for more data but takes longer
 
+MSG = True
+
 if len(argv) > 3: # Command line control for mode
     mode = argv[3]
 
+if len(argv) > 4: # Command line control for silent
+    if argv[4] == "silent":
+        MSG = False
+
+
 size = 1 # Image2File only supports 1
+counter = 0
 
-
+def msg(content):
+    if(MSG):
+        print(content)
 
 if mode == "FHD":
     w = 1920
@@ -41,6 +51,7 @@ if mode == "4K":
 if mode == "8K":
     w = 7680
     h = 4320
+maxpixels = w * h
 
 def AppendC(c):
     newimg = Image.new('RGB', (size, size), c)
@@ -54,21 +65,28 @@ def AppendC(c):
     return result
 
 main = Image.new('RGB', (h, w))
-print("MODE: "+mode)
+msg("MODE: "+mode)
 with open(fname, 'r') as f:
     content = f.read().encode("utf-8").hex() # File to hex
 
 #Convert from HEX into HEX colors
-print("Converting into colors...")
+msg("Converting into colors...")
 lines = [content[i:i+6] for i in range(0, len(content), 6)]
 for line in lines:
     l = len(line)
     if l < 6:
         line += ('0'*(6 - l)) # Padding
     c = '#' + line
-    print(c)
+    msg(c)
+    counter += 1
+    if(counter == maxpixels):
+        counter2 += 1
+        msg("Saving image "+counter2)
+        main.save(opname+"_"+counter2)
+        main = Image.new('RGB', (h, w))
     main = AppendC(c) # Append color into image
 
-print("Saving image...")
+msg("Saving final image...")
 main.save(opname)
-print("Done :-)")
+msg("Done :-)")
+
